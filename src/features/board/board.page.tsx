fac8e261dc5b5c0072244edd113da492069fddf3
pvntheraxxx <pvntheraxxx@gmail.com>
2025-06-05 17:38:02 +0300

    started the project

---
+++ src/features/board/board.page.tsx
@@ -0,0 +1,10 @@
+import { ROUTES } from "@/shared/model/routes";
+import { useParams } from "react-router-dom";
+import type { PathParams } from "@/shared/model/routes";
+
+function BoardPage() {
+  const params = useParams<PathParams[typeof ROUTES.BOARD]>();
+  return <div>Board page {params.boardId}</div>;
+}
+
+export const Component = BoardPage;
